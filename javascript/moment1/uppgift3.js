f1304750fc927b3735bf13744e8dd075cd8c7e63
Robert Eriksson <robert.eriksson@visma.com>
2023-08-30 16:17:57 +0200

    uppgift 3 & 4

---
+++ javascript/moment1/uppgift3.js
@@ -0,0 +1,11 @@
+/**
+ * Written by Robert Eriksson
+ * This just prints the multiplication table
+ * for the number 8
+ */
+
+const number = 8;
+
+for (let i = 0; i <= 10; i++) {
+    console.log(number + " * " + i + " = " + number * i)
+}
---
+++ javascript/moment1/uppgift4.js
@@ -0,0 +1,11 @@
+/**
+ * Written by Robert Eriksson
+ * this prints every number that is uneven
+ * between the numbers 3 and 23.
+ */
+
+for (i = 3; i <= 23; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
