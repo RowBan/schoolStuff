e5b9075261d1d2e81c331930c454978e4b158e09
Robert Eriksson <robert.eriksson89@hotmail.com>
2023-08-30 09:54:41 +0200

    First commit

---
+++ javascript/moment1/uppgift1.js
@@ -0,0 +1,10 @@
+/**
+ * Written by Robert Eriksson
+ * It simply prints first name, surname and email.
+ **/
+
+let firstName = "Ree";
+let surName = "Putitinputin";
+let email = "nitup.rimidalv@something.ru";
+
+console.log(firstName + " " + surName + ", " + email);
---
+++ javascript/moment1/uppgift2.js
@@ -0,0 +1,12 @@
+/**
+ * Written by Robert Eriksson
+ * It's just an exercise to test datatypes
+ * and write them to console  
+ **/
+
+let health = 100;
+let damage = 63.875;
+let damageTaken = true;
+let healthWarning = "Your health is below 50%";
+
+console.log(health + ", " + damage + ", " + damageTaken + ", " + healthWarning);
