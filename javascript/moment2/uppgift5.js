19f35913698848120fc2e66145a745fdadc6b39f
Robert Eriksson <robert.eriksson@visma.com>
2023-09-04 12:25:34 +0200

    smol changes to repo

--- javascript/moment2/uppgift5.js
+++ javascript/moment2/uppgift5.js
@@ -3,16 +3,3 @@
  * calculates sum and median of
  * array with integers
  */
-let arr = [];
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-readline.question("Enter integers: ", num => {
-    arr = num;
-    readline.close();
-})
-
-
-console.log(arr);
