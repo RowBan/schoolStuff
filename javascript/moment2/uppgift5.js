16988b1514323770ec22978e31facb261c71ed06
Robert Eriksson <robert.eriksson@visma.com>
2023-09-06 15:44:14 +0200

    uppgift5

--- javascript/moment2/uppgift5.js
+++ javascript/moment2/uppgift5.js
@@ -3,3 +3,17 @@
  * calculates sum and median of
  * array with integers
  */
+
+function mathStuff() {
+    let arr = [51, 67, -33, 5, 1, 87, 900, 2, -3, -363, 18];
+    let sum = 0;
+    let median = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        sum += arr[i]; // The same as sum = sum + arr[i]
+    }
+    median = sum / arr.length;
+    console.log("Totalsumma: " + sum + "\n" + "Medelvärde: " + median);
+}
+
+mathStuff();
